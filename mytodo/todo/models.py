6c5c419a8026f5531bf6a940644af45301703f5d
SGGM <gleb_somov@mail.ru>
2021-10-04 14:35:00 +0300

    Added todo model, view, template, registered at admin

--- mytodo/todo/models.py
+++ mytodo/todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Todo(models.Model):
+    """Basic todo model"""
+    title = models.CharField(max_length=80, blank=False, help_text="Todo title")
+    body = models.TextField(max_length=300, blank=False)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
